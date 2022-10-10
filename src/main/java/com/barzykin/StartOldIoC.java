f5d6d3c718280f2952984056ee19f4b67641ab10
Alexej_Barzykin <Alexej_Barzykin@epam.com>
2022-10-11 00:48:19 +0400

    04-spring-xml-lifecycle-default

--- src/main/java/com/barzykin/StartOldIoC.java
+++ src/main/java/com/barzykin/StartOldIoC.java
@@ -8,14 +8,14 @@ import java.util.List;
 
 public class StartOldIoC {
     public static void main(String[] args) {
-        Student john = new Student(1, "John", List.of(8, 9, 10));
-        Student bob = new Student(2, "Bob", List.of(7, 8, 9));
+//        Student john = new Student(1, "John", List.of(8, 9, 10));
+//        Student bob = new Student(2, "Bob", List.of(7, 8, 9));
 
-        Teacher alex = new Teacher(1, "Alex", 1000);
-
-        Group java = new Group(1, "Java", alex, List.of(john, bob));
-
-        java.print();
+//        Teacher alex = new Teacher(1, "Alex", 1000);
+//
+//        Group java = new Group(1, "Java", alex, List.of(john, bob));
+//
+//        java.print();
 
     }
 }
--- src/main/java/com/barzykin/model/Student.java
+++ src/main/java/com/barzykin/model/Student.java
@@ -7,6 +7,7 @@ import lombok.NoArgsConstructor;
 import lombok.ToString;
 
 import java.util.List;
+import java.util.Map;
 
 
 @EqualsAndHashCode(callSuper = true)
@@ -14,17 +15,17 @@ import java.util.List;
 @Getter
 @NoArgsConstructor
 public class Student extends Person {
-    private List<Integer> marks;
+    private Map<String, Integer> marks;
 
     public Student(int id, String name) {
         super(id, name);
     }
 
-    public void setMarks(List<Integer> marks) {
+    public void setMarks(Map<String, Integer> marks) {
         this.marks = marks;
     }
 
-    public Student(int id, String name, List<Integer> marks) {
+    public Student(int id, String name, Map<String, Integer> marks) {
         super(id, name);
         this.marks = marks;
     }
