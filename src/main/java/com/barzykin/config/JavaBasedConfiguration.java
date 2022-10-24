cd17eae71be0bfb66b9163f8404daf90f7eed84a
Alexej_Barzykin <Alexej_Barzykin@epam.com>
2022-10-25 01:01:48 +0400

    19-spring-annotation-java-based-configuration-beans

--- src/main/java/com/barzykin/config/JavaBasedConfiguration.java
+++ src/main/java/com/barzykin/config/JavaBasedConfiguration.java
@@ -6,43 +6,57 @@ import com.barzykin.model.Mentor;
 import com.barzykin.model.Student;
 import com.barzykin.model.Teacher;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 import java.util.List;
 import java.util.Map;
 
 @Configuration
+@PropertySource({"classpath:john.properties", "classpath:bob.properties", "classpath:teachers.properties"})
 public class JavaBasedConfiguration {
+    @Value("${john.id}")
+    private int johnId;
+    @Value("${john.name}")
+    private String johnName;
+    @Value("#{${john.marks}}")
+    private Map<String, Integer> johnMarks;
 
     @Bean
     public Map<String, Integer> johnMarks() {
-        return Map.of("C", 8, "Python", 9, "Java", 10);
+        return johnMarks;
     }
 
     @Bean
-    public Map<String, Integer> bobMarks() {
-        return Map.of("C", 7, "Python", 8, "Java", 9);
+    public Map<String, Integer> bobMarks(@Value("#{${bob.marks}}") Map<String, Integer> marks) {
+        return marks;
     }
 
     @Bean
     public Student john(@Autowired Map<String, Integer> johnMarks) {
-        return new Student(1, "John", johnMarks);
+        return new Student(johnId, johnName, johnMarks);
     }
 
     @Bean
-    public Student bob(@Autowired Map<String, Integer> bobMarks) {
+    public Student bob(@Value("${bob.id}") int id,
+                       @Value("${bob.name}") String name,
+                       @Autowired Map<String, Integer> bobMarks) {
         Student student = new Student();
-        student.setId(2);
-        student.setName("Bob");
+        student.setId(id);
+        student.setName(name);
         student.setMarks(bobMarks);
         return student;
     }
 
     @Bean
-    public Employee alex() {
-        Mentor alex = new Mentor(1, "Alex", 1000);
-        alex.setCertificates(List.of("Java", "AWS", "Cisco"));
+    public Employee alex(@Value("${alex.id}") int id,
+                         @Value("${alex.name}") String name,
+                         @Value("${alex.salary}") int salary,
+                         @Value("${alex.certs}") List<String> certificates) {
+        Mentor alex = new Mentor(id, name, salary);
+        alex.setCertificates(certificates);
         return alex;
     }
 
