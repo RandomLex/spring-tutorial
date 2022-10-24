4cb97d1c9eab12d690a14e632f916238c7202d16
Alexej_Barzykin <Alexej_Barzykin@epam.com>
2022-10-25 00:41:37 +0400

    19-spring-annotation-java-based-configuration-beans

--- src/main/java/com/barzykin/config/JavaBasedConfiguration.java
+++ src/main/java/com/barzykin/config/JavaBasedConfiguration.java
@@ -2,8 +2,10 @@ package com.barzykin.config;
 
 import com.barzykin.model.Employee;
 import com.barzykin.model.Group;
+import com.barzykin.model.Mentor;
 import com.barzykin.model.Student;
 import com.barzykin.model.Teacher;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -12,30 +14,48 @@ import java.util.Map;
 
 @Configuration
 public class JavaBasedConfiguration {
+
     @Bean
-    public Student john() {
-        return new Student(1, "John", Map.of("C", 8, "Python", 9, "Java", 10));
+    public Map<String, Integer> johnMarks() {
+        return Map.of("C", 8, "Python", 9, "Java", 10);
     }
 
     @Bean
-    public Student bob() {
+    public Map<String, Integer> bobMarks() {
+        return Map.of("C", 7, "Python", 8, "Java", 9);
+    }
+
+    @Bean
+    public Student john(@Autowired Map<String, Integer> johnMarks) {
+        return new Student(1, "John", johnMarks);
+    }
+
+    @Bean
+    public Student bob(@Autowired Map<String, Integer> bobMarks) {
         Student student = new Student();
         student.setId(2);
         student.setName("Bob");
-        student.setMarks(Map.of("C", 7, "Python", 8, "Java", 9));
+        student.setMarks(bobMarks);
         return student;
     }
 
     @Bean
     public Employee alex() {
-        Teacher alex = new Teacher(1, "Alex", 1000);
+        Mentor alex = new Mentor(1, "Alex", 1000);
         alex.setCertificates(List.of("Java", "AWS", "Cisco"));
         return alex;
     }
 
     @Bean
-    public Group java() {
-        Group javaEe = new Group(1, "Java EE", alex(), List.of(john(), bob()));
+    public Employee anton() {
+        Teacher alex = new Teacher(2, "Anton", 800);
+        alex.setCertificates(List.of("Java", "JEE"));
+        return alex;
+    }
+
+    @Bean
+    public Group java(@Autowired Employee alex, @Autowired Student john, @Autowired Student bob) {
+        Group javaEe = new Group(1, "Java EE", alex, List.of(john, bob));
         return javaEe;
     }
 
